c6d1ba3ed16d386a7d0e3a2b6ec3b52c48f05854
Cole Strickler <colestrickler@gmail.com>
2022-05-08 17:14:54 -0500

    finished for now

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-import re
-
-phone = re.compile('\+?[0-9]?-?\(?[0-9]*\)?-[0-9]+-[0-9]+')
-
-response = requests.get("https://www.ups.com/us/en/support/contact-us.page")
-search = re.findall(phone, response.text)
-print(response.text)
-print(search)
